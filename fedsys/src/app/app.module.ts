d02a541df7e0618388e9212b97b31dbb91e980b7
VictorDC7 <victordc704@icloud.com>
2019-08-28 13:43:14 -0400

    Proyecto angular

---
+++ fedsys/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TableComponent } from './componets/table/table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    TableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
