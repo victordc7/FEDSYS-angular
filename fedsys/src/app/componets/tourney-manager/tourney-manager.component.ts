621df09cded61f31c539932b53d1501d077605e5
jose ortiz <jottache20@gmail.com>
2020-01-08 17:12:41 -0400

    vista de tourney manager

--- fedsys/src/app/componets/tourney-manager/tourney-manager.component.ts
+++ fedsys/src/app/componets/tourney-manager/tourney-manager.component.ts
@@ -9,12 +9,66 @@ import { ServiceService } from 'src/app/service.service';
 })
 
 export class TourneyManagerComponent implements OnInit {
-
+  subcategories = []
+  tournament = []
   dataOrg = {};
 
   constructor(private serverService: ServiceService) { }
 
   ngOnInit() {
+
+    const body = {
+      query: `
+      query{
+        tourneyById (_id: "5e1634a63282b10355c66652"){
+          _id
+          name
+          subcategories{
+            _id
+            name
+            code
+            parent{
+              _id
+              name
+            }
+          }
+          competitors{
+            firstName
+            lastName
+            personalID
+            age
+            gender
+            city
+            subcategory {
+              code
+              name
+              parent {
+                _id
+              }
+            }
+            email
+            phone
+          }
+          judges{
+            firstName
+            lastName
+            personalID
+            email
+            age
+            city
+          }
+        }
+      }`
+    }
+
+    this.serverService.graphql(body)
+    .subscribe(res => {
+      console.log(res)
+      this.subcategories.push(res['data']['tourneyById']['subcategories'])
+      this.tournament.push(res['data']['tourneyById'])
+      console.log(this.subcategories)
+      console.log(this.tournament)
+    })
   }
 
   nextFase() {
